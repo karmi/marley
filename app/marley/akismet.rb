7d1edcb7666c2a644021ff289430f474c4c3afe7
Karel Minarik <karmi@karmi.cz>
2008-10-27 11:29:09 +0100

    Refactored blog engine into proper files from the mess

---
+++ app/marley/akismet.rb
@@ -0,0 +1,15 @@
+require File.join(File.dirname(__FILE__), '..', '..', 'vendor/akismet')  # ... filter spam from ham
+
+module Marley
+
+  # = Interface to Akismet checking library
+  # TODO : Cleanup
+  class Akismet
+    def self.check( ip, user_agent, referrer, permalink, comment_type, author, email, url, body, other )
+      akismet = ::Akismet.new(::CONFIG['akismet']['key'], ::CONFIG['akismet']['url'])
+      raise ArgumentError, "Invalid Akismet key" unless akismet.verifyAPIKey
+      akismet.commentCheck(ip, user_agent, referrer, permalink, comment_type, author, email, url, body, other)
+    end
+  end
+  
+end
---
+++ app/marley/comment.rb
@@ -0,0 +1,30 @@
+module Marley
+
+  DATA_DIRECTORY = File.join(File.dirname(__FILE__), '..', '..', 'data') unless defined? DATA_DIRECTORY
+
+  # = Comments for articles
+  # .db file is created in Marley::DATA_DIRECTORY
+  class Comment < ActiveRecord::Base
+    
+    ActiveRecord::Base.establish_connection( :adapter => 'sqlite3', :database => File.join(DATA_DIRECTORY, 'comments.db') )
+    
+    belongs_to :post
+
+    validates_presence_of :author, :email, :body, :post_id
+
+    before_create :check_spam
+    
+    private
+    
+    def check_spam
+      result = Marley::Akismet.check( self.ip, self.user_agent, self.referrer, nil, 'comment', self.author, self.email, self.url, self.body, nil )
+      puts result.inspect
+      if result
+        self.checked = true
+        self.spam = result
+      end
+    end
+    
+  end
+
+end

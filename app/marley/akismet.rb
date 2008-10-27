fe7ce8421ea2dda0f92f27897add325723283eb5
Karel Minarik <karmi@karmi.cz>
2008-10-27 19:26:42 +0100

    Implement new Akismet interface, Akismetor from http://railscasts.com/episodes/65-stopping-spam-with-akismet

--- app/marley/akismet.rb
+++ app/marley/akismet.rb
@@ -1,14 +1,19 @@
-require File.join(File.dirname(__FILE__), '..', '..', 'vendor/akismet')  # ... filter spam from ham
+require File.join(File.dirname(__FILE__), '..', '..', 'vendor/akismet')    # ... filter spam from ham
+require File.join(File.dirname(__FILE__), '..', '..', 'vendor/akismetor')  # ... filter spam from ham
 
 module Marley
 
   # = Interface to Akismet checking library
   # TODO : Cleanup
   class Akismet
-    def self.check( ip, user_agent, referrer, permalink, comment_type, author, email, url, body, other )
-      akismet = ::Akismet.new(::CONFIG['akismet']['key'], ::CONFIG['akismet']['url'])
-      raise ArgumentError, "Invalid Akismet key" unless akismet.verifyAPIKey
-      akismet.commentCheck(ip, user_agent, referrer, permalink, comment_type, author, email, url, body, other)
+    # def self.check( ip, user_agent, referrer, permalink, comment_type, author, email, url, body, other )
+    #   akismet = ::Akismet.new(::CONFIG['akismet']['key'], ::CONFIG['akismet']['url'])
+    #   raise ArgumentError, "Invalid Akismet key" unless akismet.verifyAPIKey
+    #   akismet.commentCheck(ip, user_agent, referrer, permalink, comment_type, author, email, url, body, other)
+    # end
+
+    def self.check
+      
     end
   end
   
--- app/marley/comment.rb
+++ app/marley/comment.rb
@@ -13,14 +13,33 @@ module Marley
     before_create :check_spam
     
     private
+
+    # See http://railscasts.com/episodes/65-stopping-spam-with-akismet
+    def akismet_attributes
+      {
+        :key                  => CONFIG['akismet']['key'],
+        :blog                 => CONFIG['akismet']['url'],
+        :user_ip              => self.ip,
+        :user_agent           => self.user_agent,
+        :comment_author       => self.author,
+        :comment_author_email => self.email,
+        :comment_author_url   => self.url,
+        :comment_content      => self.body
+      }
+    end
     
     def check_spam
-      result = Marley::Akismet.check( self.ip, self.user_agent, self.referrer, nil, 'comment', self.author, self.email, self.url, self.body, nil )
-      puts result.inspect
-      if result
-        self.checked = true
-        self.spam = result
-      end
+      # result = Marley::Akismet.check( self.ip, self.user_agent, self.referrer, nil, 'comment', self.author, self.email, self.url, self.body, nil )
+      #       puts result.inspect
+      #       if result
+      #         self.checked = true
+      #         self.spam = result
+      #       end
+      result = !Akismetor.spam?(akismet_attributes)
+      puts result
+      self.checked = true
+      self.spam = result
+      true # return true so it doesn't stop save
     end
     
   end

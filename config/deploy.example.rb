59b20d8ca0eda0a16efbe4e4ae1816de056c0515
Karel Minarik <karmi@karmi.cz>
2008-11-09 00:04:22 +0100

    Added Capistrano task for setting-up remote repository for articles with "autoupdating" post-receive hook

--- config/deploy.example.rb
+++ config/deploy.example.rb
@@ -1,3 +1,6 @@
+# Load Marley configuration
+CONFIG = YAML.load_file( File.join(File.dirname(__FILE__), 'config.yml') ) unless defined? CONFIG
+
 # ----- Setup SSH -------------------------------------------------------------
 set :user, "{REPLACE WITH YOUR SSH USERNAME}"
 # -----------------------------------------------------------
@@ -22,27 +25,51 @@ role :app, "{REPLACE WITH YOUR SERVER}"
 role :web, "{REPLACE WITH YOUR SERVER}"
 role :db,  "{REPLACE WITH YOUR SERVER}", :primary => true
 
-# ----- Specific tasks --------------------------------------------------------
+# ----- Marley tasks ----------------------------------------------------------
 
-namespace :data do
-  task :sync, :roles => :app do
-    # TODO
-    # upload "../data", "#{deploy_to}/../data"
+namespace :sync do
+  namespace :setup do
+    task :default do
+      upload_data_repository
+      add_post_receive_hook_for_data_repository
+      add_git_remote_to_data_directory
+    end
+    task :upload_data_repository do
+      top.upload(File.join(CONFIG['data_directory'], '.git'), "#{deploy_to}/articles.git" )
+      puts "--- Uploaded Git repository from data directory to '#{deploy_to}/articles.git'\n"
+      run "cd #{deploy_to}; git clone articles.git #{CONFIG['data_directory'].split('/').last}"
+      puts "--- Initialized Git repository\n"
+    end
+    task :add_post_receive_hook_for_data_repository do
+      post_receive_script   = "#{deploy_to}/articles.git/hooks/post-receive"
+      remote_data_directory = File.join(deploy_to, CONFIG['data_directory'].split('/').last)
+      hook_command ="export GIT_DIR=.git; cd #{remote_data_directory}; git pull origin master; echo \"[Post-receive] Data directory has been synchronized in #{remote_data_directory}\""
+      run "chmod +x #{post_receive_script}; echo '#{hook_command}' >> #{post_receive_script}"
+      puts "--- Added post-receive hook for Git repository\n"
+    end
+    task :add_git_remote_to_data_directory do
+      `cd #{CONFIG['data_directory']}; git remote add sync #{user}@#{roles[:app].instance_variable_get(:@static_servers).first.instance_variable_get(:@host)}:#{deploy_to}/articles.git`
+      puts "--- Added remote repository 'sync' for data. Use 'git push sync' to synchronize your content.\n"
+    end
   end
 end
 
-desc "Upload configuration file (config/config.yml) to deploy"
-task :upload_config, :roles => :app do
- top.upload('config/config.yml', "#{shared_path}/config.yml" )
+namespace :app do
+  desc "Upload configuration file (config/config.yml) to deploy"
+  task :upload_config, :roles => :app do
+   top.upload('config/config.yml', "#{shared_path}/config.yml" )
+  end
 end
 
 # ----- Hooks ----------------------------------------------------------------
+
+before "deploy:cold" do
+  app.upload_config
+end
+
 after "deploy:update_code" do
   # run "ln -nfs #{shared_path}/sinatra #{release_path}/sinatra"
   run "ln -nfs #{shared_path}/config.yml #{release_path}/config/config.yml"
-end
-after "deploy:cold" do
-  run "cd #{current_path}; rake app:install:create_data_directory; rake app:install:create_database; rake app:install:create_sample_article"
 end
 
 # ----- Over-ride deploy tasks ------------------------------------------------
@@ -85,4 +112,5 @@ namespace :deploy do
   task :stop, :roles => :app do
      run "cd #{current_path}; rake server:stop"
   end
+
 end
